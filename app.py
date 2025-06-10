6d410c48f2e4044502834f74ac77b8589800e6c9
Yogesh T22 <yogeshoffy@gmail.com>
2025-06-10 17:53:40 +0530

    update app.py with new message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from my CI/CD Pipeline! Version 1'
+    return 'This is updated message for second time run!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
