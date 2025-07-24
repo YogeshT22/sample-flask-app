af12e447563b6f90af69ed9211d486351c63670f
Yogesh T22 <yogeshoffy@gmail.com>
2025-07-24 17:09:41 +0530

    feat(k8s): finalize kubernetes manifests and app updates

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'finally gitea webhooks working 201 request!'
+def show_msg():
+    return 'finally gitea webhooks working 201 request!, K3d - Kubernetes running!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
